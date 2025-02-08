fdaac23b7c75368ad6b6d7547173f92c2033ec67
shahidul shohid <shahidulislamshohid7@gmail.com>
2025-02-08 21:59:42 +0600

    middleware some funtionality is done

--- src/middleware.js
+++ src/middleware.js
@@ -1,15 +1,31 @@
-
-
 import { NextResponse } from "next/server";
 
 export function middleware(request) {
+    let currentCookies = request.cookies.get('nextjs-token')
+    console.log(currentCookies.value)
   // Debugging the exact pathname
   const dummyUserData = {
-    role: "admin", 
-    email: "test@admin.com"
+    role: "user",
+    email: "test@admin.com",
+  };
+
+  let isServicesPage = request.nextUrl.pathname.startsWith("/services");
+  let isAdmin = dummyUserData.role === "admin";
+  if (isServicesPage && !isAdmin) {
+    //ata login page niye jay and url o change hoye login hoy==
+    return NextResponse.redirect(new URL("/login", request.url));
+
+    // ata login page niye jay but url change hoy na ===
+    // return NextResponse.rewrite(new URL('/login', request.url))
   }
-  let isServicesPage = request.nextUrl.pathname.startsWith('/services')
-  console.log("Checking if it starts with '/services============':", isServicesPage);
+  console.log(
+    "Checking if it starts with '/services============':",
+    isServicesPage
+  );
 
   return NextResponse.next();
 }
+// See "Matching Paths" below to learn more
+// export const config = {
+//   matcher: "/about/:path*",
+// };
